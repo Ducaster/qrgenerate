6231f4e9a2397fea7f22b59a56c854e588c7045f
Higashino-Keigo <kooa3399@kakao.com>
2023-12-29 11:54:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+import styled from 'styled-components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  let [name, setName] = useState("");
+  let [region, setRegion] = useState("");
+  let [qrValue, setQrValue] = useState("");
+
+  const generateQR = () => {
+    if (!name || !region) {
+      toast.error("이름과 지역을 모두 입력해주세요.");
+    } else {
+      setQrValue(JSON.stringify({ name, region }));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <input onChange = {e => setName(e.target.value)} type="text" placeholder="이름" /> <br></br>
+      <select onChange = {e => setRegion(e.target.value)} value={region}>
+        <option value="" disabled hidden>지역 선택</option>
+        <option value="대학">대학</option>
+        <option value="중랑">중랑</option>
+        <option value="성북">성북</option>
+        <option value="노원">노원</option>
+        <option value="수락">수락</option>
+        <option value="도봉">도봉</option>
+        <option value="새신자">새신자</option>
+      </select> <br></br>
+      <button onClick={generateQR}>Generate</button>
+      <br></br>
+      {qrValue && <QRCode value= {qrValue} size={300} />}
+      <ToastContainer />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
